5696e644ec45faa2ad6dd7d1a40b3c8450bda14c
Alexander Popel <alexander.popel@postindustria.com>
2025-05-21 13:28:55 +0300

    ci: fix paths in update-packages

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -16,8 +16,8 @@ try {
 
     Write-Output "Generating amalgamation..."
     $src = "ip-intelligence-cxx/src"
-    awk -f ci/amalgamate.awk $src/fiftyone.h $src/ip-graph-cxx/graph.h >dd/ip-intelligence-cxx.h
-    awk -f ci/amalgamate.awk $src/common-cxx/*.c $src/ip-graph-cxx/*.c $src/*.c >dd/ip-intelligence-cxx.c
+    awk -f ci/amalgamate.awk $src/fiftyone.h $src/ip-graph-cxx/graph.h >ipi_interop/ip-intelligence-cxx.h
+    awk -f ci/amalgamate.awk $src/common-cxx/*.c $src/ip-graph-cxx/*.c $src/*.c >ipi_interop/ip-intelligence-cxx.c
 } finally {
     Write-Output "Cleaning up..."
     Remove-Item -Recurse -Force ip-intelligence-cxx
