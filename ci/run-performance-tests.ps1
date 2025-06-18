f18a1e33b42c636b87628f180f3a8ce31e4b2d0a
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-06-18 11:12:16 +0300

    Merged Pull Request '#6 enable-performance-tests->main: ci: enable performance tests'

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,2 +1,8 @@
-# TODO: fix
-Write-Warning "No performance tests yet"
+param (
+    [Parameter(Mandatory)][string]$RepoName,
+    [Parameter(Mandatory)][string]$OrgName,
+    [Parameter(Mandatory)][string]$Name,
+    [string]$Branch = "main"
+)
+
+./go/run-performance-tests.ps1 @PSBoundParameters -ExamplesRepo ip-intelligence-go-examples -Example ./ipi_onpremise/performance/performance.go
