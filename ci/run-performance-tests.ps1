a771e71720db3a4eacdbf09fd02236a02a32949f
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-06-18 15:17:56 +0300

    Merged Pull Request '#7 enable-performance-tests->main: ci: fetch assets for tests, enable performance test'

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -5,4 +5,5 @@ param (
     [string]$Branch = "main"
 )
 
+$env:DATA_FILE = "$PWD/assets/51Degrees-EnterpriseIpiV41.ipi"
 ./go/run-performance-tests.ps1 @PSBoundParameters -ExamplesRepo ip-intelligence-go-examples -Example ./ipi_onpremise/performance/performance.go
