b12eed6e3a36c9120b9877b755d1cfa041f951d4
Automation51D <51DCI@51Degrees.com>
2025-06-21 02:33:23 +0000

    Update packages

--- ipi_interop/ip-intelligence-cxx.c
+++ ipi_interop/ip-intelligence-cxx.c
@@ -20808,7 +20808,7 @@ static uint64_t extractValue(
 		result = extractSubValue(
 			*source,
 			bitIndex,
-			(bitsAvailable < recordSize) ? bitsAvailable : recordSize);
+			(bitsAvailable < recordSize) ? bitsAvailable : (uint8_t)recordSize);
 	}
 	int remainingBits = recordSize + bitIndex - 8;
 
@@ -20820,7 +20820,7 @@ static uint64_t extractValue(
 
 	if (remainingBits > 0) {
 		result <<= remainingBits;
-		result |= extractSubValue(*nextByte, 0, remainingBits);
+		result |= extractSubValue(*nextByte, 0, (uint8_t)remainingBits);
 	}
 
 	return result;
