37a9606df88e9199d9764654be6780ef9de7ff78
Automation51D <51DCI@51Degrees.com>
2025-06-23 11:52:50 +0000

    Update packages

--- ipi_interop/ip-intelligence-cxx.h
+++ ipi_interop/ip-intelligence-cxx.h
@@ -9038,6 +9038,201 @@ static const uint8_t fiftyoneDegreesDefaultWktDecimalPlaces = 16;
 
 #endif //FIFTYONE_DEGREES_CONSTANTSIPI_H
 /* *********************************************************************
+* This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2025 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence (EUPL)
+ * v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+#ifndef FIFTYONE_DEGREES_IPI_WEIGHTED_RESULTS_INCLUDED
+#define FIFTYONE_DEGREES_IPI_WEIGHTED_RESULTS_INCLUDED
+
+/**
+ * @file ipi_weighted_results.h
+ * @brief Defines structures and functions for handling weighted values in IP Intelligence results.
+ * 
+ * This file provides the data structures and functions needed to work with weighted values
+ * of different types (int, double, bool, byte, string) in the IP Intelligence system.
+ * Weighted values include both the actual value and a weighting that indicates the
+ * confidence or importance of that value.
+ */
+
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+#ifndef FIFTYONE_DEGREES_BOOL_INCLUDED
+#define FIFTYONE_DEGREES_BOOL_INCLUDED
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#define EXTERNAL extern "C"
+#else
+#define EXTERNAL
+#endif
+
+/**
+ * Convert integer to bool type. This is mainly to support
+ * language that do not support function to cast to C bool
+ * type
+ */
+EXTERNAL bool fiftyoneDegreesIntToBool(int i);
+
+/**
+ * Convert bool to integer type. This is mainly to support
+ * language that do not suport function to cast between C
+ * bool and integer type.
+ */
+EXTERNAL int fiftyoneDegreesBoolToInt(bool b);
+
+#endif
+
+/**
+ * @brief Header structure for all weighted value types.
+ * 
+ * This structure serves as the common header for all weighted value types,
+ * containing the value type, property index, and raw weighting information.
+ */
+typedef struct fiftyone_degrees_weighted_value_header_t {
+ fiftyoneDegreesPropertyValueType valueType; /**< The type of the property value */
+ int requiredPropertyIndex;                  /**< Index of the required property */
+ uint16_t rawWeighting;                      /**< Raw confidence weighting value */
+} fiftyoneDegreesWeightedValueHeader;
+
+
+/**
+ * @brief Structure for weighted integer values.
+ * 
+ * Contains a weighted value header and an integer value.
+ */
+typedef struct fiftyone_degrees_weighted_int_t {
+ fiftyoneDegreesWeightedValueHeader header; /**< Common header for all weighted values */
+ int32_t value;                             /**< The integer value */
+} fiftyoneDegreesWeightedInt;
+
+/**
+ * @brief Structure for weighted double values.
+ * 
+ * Contains a weighted value header and a double value.
+ */
+typedef struct fiftyone_degrees_weighted_double_t {
+ fiftyoneDegreesWeightedValueHeader header; /**< Common header for all weighted values */
+ double value;                              /**< The double value */
+} fiftyoneDegreesWeightedDouble;
+
+/**
+ * @brief Structure for weighted boolean values.
+ * 
+ * Contains a weighted value header and a boolean value.
+ */
+typedef struct fiftyone_degrees_weighted_bool_t {
+ fiftyoneDegreesWeightedValueHeader header; /**< Common header for all weighted values */
+ bool value;                                /**< The boolean value */
+} fiftyoneDegreesWeightedBool;
+
+/**
+ * @brief Structure for weighted byte values.
+ * 
+ * Contains a weighted value header and a byte value.
+ */
+typedef struct fiftyone_degrees_weighted_byte_t {
+ fiftyoneDegreesWeightedValueHeader header; /**< Common header for all weighted values */
+ uint8_t value;                             /**< The byte value */
+} fiftyoneDegreesWeightedByte;
+
+/**
+ * @brief Structure for weighted string values.
+ * 
+ * Contains a weighted value header, string data, and a pointer to the string value.
+ * The stringData field owns the memory for the value.
+ */
+typedef struct fiftyone_degrees_weighted_string_t {
+ fiftyoneDegreesWeightedValueHeader header; /**< Common header for all weighted values */
+ fiftyoneDegreesData stringData;            /**< Data structure that owns the string memory */
+ const char *value;                         /**< Pointer to the string value */
+} fiftyoneDegreesWeightedString;
+
+
+/**
+ * @brief Collection of weighted values.
+ * 
+ * This structure holds a collection of weighted values of various types.
+ * It manages the memory for both the values themselves and the array of pointers to them.
+ */
+typedef struct fiftyone_degrees_weighted_values_collection_t {
+ fiftyoneDegreesData valuesData;                    /**< Data structure that owns the actual values */
+ fiftyoneDegreesData itemsData;                     /**< Data structure that owns the items table of contents */
+ fiftyoneDegreesWeightedValueHeader ** items;       /**< Array of pointers to weighted value headers */
+ uint32_t itemsCount;                               /**< Number of items in the collection */
+} fiftyoneDegreesWeightedValuesCollection;
+
+
+/**
+ * @brief Gets a collection of weighted values from IP Intelligence results.
+ * 
+ * This function extracts weighted values for specified properties from the results
+ * and returns them as a collection.
+ * 
+ * @param results Pointer to the IP Intelligence results
+ * @param requiredPropertyIndexes Array of required property indexes to extract
+ * @param requiredPropertyIndexesLength Number of indexes in the requiredPropertyIndexes array
+ * @param tempData Temporary data structure for string conversion operations
+ * @param exception Pointer to an exception structure for error handling
+ * @return A collection of weighted values
+ */
+EXTERNAL fiftyoneDegreesWeightedValuesCollection fiftyoneDegreesResultsIpiGetValuesCollection(
+ fiftyoneDegreesResultsIpi *results,
+ const int *requiredPropertyIndexes,
+ uint32_t requiredPropertyIndexesLength,
+ fiftyoneDegreesData *tempData,
+ fiftyoneDegreesException* exception);
+
+/**
+ * @brief Releases resources used by a weighted values collection.
+ * 
+ * This function frees all memory allocated for the collection and its items.
+ * 
+ * @param collection Pointer to the collection to release
+ */
+EXTERNAL void fiftyoneDegreesWeightedValuesCollectionRelease(
+ fiftyoneDegreesWeightedValuesCollection *collection);
+
+#endif //FIFTYONE_DEGREES_IPI_WEIGHTED_RESULTS_INCLUDED
+/* *********************************************************************
  * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
  * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
  * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
@@ -9118,54 +9313,6 @@ static const uint8_t fiftyoneDegreesDefaultWktDecimalPlaces = 16;
 #else
 // Other non-mingw the default Snprintf is standard conforming.
 #define Snprintf snprintf
-#endif
-/* *********************************************************************
- * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
- * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
- * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
- *
- * This Original Work is licensed under the European Union Public Licence
- * (EUPL) v.1.2 and is subject to its terms as set out below.
- *
- * If a copy of the EUPL was not distributed with this file, You can obtain
- * one at https://opensource.org/licenses/EUPL-1.2.
- *
- * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
- * amended by the European Commission) shall be deemed incompatible for
- * the purposes of the Work and the provisions of the compatibility
- * clause in Article 5 of the EUPL shall not apply.
- *
- * If using the Work as, or as part of, a network application, by
- * including the attribution notice(s) required under Article 5 of the EUPL
- * in the end user terms of the application under an appropriate heading,
- * such notice(s) shall fulfill the requirements of that article.
- * ********************************************************************* */
-
-#ifndef FIFTYONE_DEGREES_BOOL_INCLUDED
-#define FIFTYONE_DEGREES_BOOL_INCLUDED
-
-#include <stdbool.h>
-
-#ifdef __cplusplus
-#define EXTERNAL extern "C"
-#else
-#define EXTERNAL
-#endif
-
-/**
- * Convert integer to bool type. This is mainly to support
- * language that do not support function to cast to C bool
- * type
- */
-EXTERNAL bool fiftyoneDegreesIntToBool(int i);
-
-/**
- * Convert bool to integer type. This is mainly to support
- * language that do not suport function to cast between C
- * bool and integer type.
- */
-EXTERNAL int fiftyoneDegreesBoolToInt(bool b);
-
 #endif
 /* *********************************************************************
  * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
@@ -9978,6 +10125,13 @@ MAP_TYPE(Ipv4Range)
 MAP_TYPE(Ipv6Range)
 MAP_TYPE(CombinationProfileIndex)
 MAP_TYPE(ResultProfileIndex)
+MAP_TYPE(WeightedValueHeader)
+MAP_TYPE(WeightedInt)
+MAP_TYPE(WeightedDouble)
+MAP_TYPE(WeightedBool)
+MAP_TYPE(WeightedByte)
+MAP_TYPE(WeightedString)
+MAP_TYPE(WeightedValuesCollection)
 
 // Methods
 #define ResultsIpiCreate fiftyoneDegreesResultsIpiCreate /**< Synonym for #fiftyoneDegreesResultsIpiCreate function. */
@@ -10004,6 +10158,8 @@ MAP_TYPE(ResultProfileIndex)
 #define IpiGetIpAddressAsString fiftyoneDegreesIpiGetIpAddressAsString /**< Synonym for #fiftyoneDegreesIpiGetIpAddressAsString function. */
 #define IpiGetIpAddressAsByteArray fiftyoneDegreesIpiGetIpAddressAsByteArray /**< Synonym for #fiftyoneDegreesIpiGetIpAddressAsByteArray function. */
 #define IpiIterateProfilesForPropertyAndValue fiftyoneDegreesIpiIterateProfilesForPropertyAndValue /**< Synonym for #fiftyoneDegreesIpiIterateProfilesForPropertyAndValue function. */
+#define ResultsIpiGetValuesCollection fiftyoneDegreesResultsIpiGetValuesCollection /**< Synonym for #fiftyoneDegreesResultsIpiGetValuesCollection function. */
+#define WeightedValuesCollectionRelease fiftyoneDegreesWeightedValuesCollectionRelease /**< Synonym for #fiftyoneDegreesWeightedValuesCollectionRelease function. */
 
 // Constants
 #define DefaultWktDecimalPlaces fiftyoneDegreesDefaultWktDecimalPlaces /**< Synonym for #fiftyoneDegreesDefaultWktDecimalPlaces config. */
