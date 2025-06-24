65a652a74d68d7a3f0d87616dcf6282902c8003d
Automation51D <51DCI@51Degrees.com>
2025-06-24 03:24:59 +0000

    Update documentation from main

--- 4.4/examples.js
+++ 4.4/examples.js
@@ -1,4 +1,6 @@
 var examples =
 [
-    [ "examples/ipi_onpremise/getting_started/getting_started.go", "examples_2ipi_onpremise_2getting_started_2getting_started_8go-example.html", null ]
+    [ "examples/onpremise/getting_started.go", "examples_2onpremise_2getting_started_8go-example.html", null ],
+    [ "examples/onpremise/offline_processing.go", "examples_2onpremise_2offline_processing_8go-example.html", null ],
+    [ "examples/onpremise/performance.go", "examples_2onpremise_2performance_8go-example.html", null ]
 ];
--- 4.4/navtreeindex0.js
+++ 4.4/navtreeindex0.js
@@ -1,7 +1,9 @@
 var NAVTREEINDEX0 =
 {
 "examples.html":[2],
-"examples_2ipi_onpremise_2getting_started_2getting_started_8go-example.html":[2,0],
+"examples_2onpremise_2getting_started_8go-example.html":[2,0],
+"examples_2onpremise_2offline_processing_8go-example.html":[2,1],
+"examples_2onpremise_2performance_8go-example.html":[2,2],
 "md__home_runner_work_ip-intelligence-go_ip-intelligence-go_common_ip-intelligence-go__r_e_a_d_m_e.html":[0],
 "md__home_runner_work_ip-intelligence-go_ip-intelligence-go_common_ip-intelligence-go_ip-intelligence-go-examples__r_e_a_d_m_e.html":[1]
 };
