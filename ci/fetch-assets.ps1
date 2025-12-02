5f108e07b7ea1b8e337ec78c4f7c3a899940f114
Alexander Popel <alexander.popel@postindustria.com>
2025-12-02 16:43:05 +0200

    ci: use common fetch-assets script

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,15 +1,6 @@
 param (
-    [Parameter(Mandatory)][string]$DeviceDetectionUrl
+    [Parameter(Mandatory)][string]$IpIntelligenceUrl
 )
 $ErrorActionPreference = "Stop"
 
-$assets = New-Item -ItemType Directory -Path assets -Force
-$dataFile = "$assets/51Degrees-EnterpriseIpiV41.ipi"
-
-if (Test-Path $assets/$dataFile) {
-    Write-Host "Data file exists, skipping download"
-} else {
-    Write-Host "Downloading data file ($dataFile.gz)..."
-    ./steps/download-data-file.ps1 -FullFilePath "$dataFile.gz" -Url $DeviceDetectionUrl
-    ./steps/gunzip-file.ps1 "$dataFile.gz"
-}
+./steps/fetch-assets.ps1 -IpIntelligenceUrl:$IpIntelligenceUrl -Assets "51Degrees-EnterpriseIpiV41.ipi"
