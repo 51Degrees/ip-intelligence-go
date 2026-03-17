b1f45cc60c8e4797cbc62d6cc63b5405b70de463
Alexander Popel <alexander.popel@postindustria.com>
2026-03-17 09:34:34 +0200

    ci: silence the `-lm` warning on macOS

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -13,6 +13,12 @@ $env:DATA_FILE = "$PWD/assets/51Degrees-EnterpriseIpiV41.ipi"
 $env:EVIDENCE_YAML = "$PWD/assets/ip-intelligence-evidence.yml"
 $env:DD_DATA_FILE = "$PWD/assets/TAC-HashV41.hash"
 
+if ($IsMacOS) {
+    # go-junit-report seems to count the harmless warning about -lm being
+    # provided multiple times as an error
+    $env:CGO_LDFLAGS = '-Wl,-no_warn_duplicate_libraries'
+}
+
 Push-Location "$PSScriptRoot/.."
 try {
     go test -v ./examples/... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$results/results.xml"
