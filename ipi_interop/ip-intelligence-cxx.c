930afe3878b91032614cd865430f48f32c304ec1
Automation51D <60396166+Automation51D@users.noreply.github.com>
2026-06-13 04:45:22 +0100

    Merged Pull Request '#48 update-packages/main->main: Update packages'
    Co-authored-by: Automation51D <51DCI@51Degrees.com>

--- ipi_interop/ip-intelligence-cxx.c
+++ ipi_interop/ip-intelligence-cxx.c
@@ -17688,7 +17688,7 @@ static StatusMessage messages[] = {
 		"pool. Another way to avoid this is by using an in-memory "
 		"configuration, which avoids using file handles completely, and "
 		"removes any limit on concurrency. For info see "
-		"https://51degrees.com/documentation/4.5/_device_detection__features__concurrent_processing.html"},
+		"https://51degrees.com/documentation/4.5/_device_detection__features__concurrent_processing.html?utm_source=code&utm_medium=comment&utm_campaign=common-cxx&utm_content=status.c&utm_term=insufficient_handles"},
 	{ COLLECTION_INDEX_OUT_OF_RANGE,
 		"Index used to retrieve an item from a collection was out of range." },
 	{ COLLECTION_OFFSET_OUT_OF_RANGE,
